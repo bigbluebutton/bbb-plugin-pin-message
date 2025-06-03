2068935e160759cceeb10eb7cca21db50af8ff71
Gabriel Ramos <gcrepho@gmail.com>
2025-06-03 10:44:53 -0300

    fix: add service to convert markdown

--- src/plugin-test/component.tsx
+++ src/plugin-test/component.tsx
@@ -9,6 +9,7 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import Styled from './styles';
+import { messageToMarkdown } from './service';
 
 interface PluginPinMessageProps {
   pluginUuid: string;
@@ -40,6 +41,7 @@ function PluginPinMessage(
   } = pluginApi.useDataChannel<PinnedMessage>('pinMessage', DataChannelTypes.LATEST_ITEM);
 
   const currentUser = pluginApi.useCurrentUser();
+  const allowedElements = ['a', 'code', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'ol', 'ul', 'p', 'strong'];
 
   useEffect(() => {
     // highlight the pinned message in the chat window
@@ -148,9 +150,10 @@ function PluginPinMessage(
             >
               <Styled.Markdown
                 linkTarget="_blank"
+                allowedElements={allowedElements}
                 unwrapDisallowed
               >
-                {lastPinnedMessage}
+                {messageToMarkdown(lastPinnedMessage)}
               </Styled.Markdown>
             </div>
             <div
